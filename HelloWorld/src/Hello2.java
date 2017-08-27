094e816520885787e21ae2deb423cd5a236672b4
Akshit Mehta <mehtaakshit7@gmail.com>
2017-08-27 11:47:29 -0700

    Hello2 Soln

--- HelloWorld/src/Hello2.java
+++ HelloWorld/src/Hello2.java
@@ -1,6 +1,9 @@
 public class Hello2 {
 
     public static void main(String[] args) {
-        System.out.println("hello again!");
+
+
+        System.out.print("hello again!");
+        System.out.print(1+2);
     }
 }
