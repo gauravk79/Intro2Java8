be77aec9f6141a75e4ab331f387900c3f2eb6de5
Akshit Mehta <mehtaakshit7@gmail.com>
2017-09-10 11:02:22 -0700

    Part 1-Part 4 solutions

--- Assignments/src/com/javalearning/Assignment1.java
+++ Assignments/src/com/javalearning/Assignment1.java
@@ -3,6 +3,7 @@ package com.javalearning;
 public class Assignment1 {
     public static void main(String[] args) {
 
+
         /*
          Part1:
           Using the single System.out.println below, replace <write answer code here> such that the output is the following 2 lines:
@@ -10,19 +11,37 @@ public class Assignment1 {
          a comment *\\
         */
 
-        System.out.println("<write answer code here>");
+        System.out.println("\\\\*This is not \na comment*\\\\");
 
         /*
         Part2:
         Demonstrate difference between operation && and & in an if/else statement
          */
+        int x = 0;
+        int y = 5;
+        if (y >= 30 && y / x > 0) {
+            System.out.println("y is bigger");
 
+        } else {
+            System.out.println("y is smaller");
+        }
         /*
         Part3:
         Demonstrate difference between operation || and | in an if/else statement
          */
 
+        int a = 0;
+        int b = 5;
+        if (b >= 3 || b / a > 0) {
+            System.out.println("b is bigger");
 
+        } else {
+            System.out.println("b is smaller");
+        }
+        displayHighScorePosition("Peter",1500);
+        displayHighScorePosition("Jen",900);
+        displayHighScorePosition("Jack",400);
+        displayHighScorePosition("James",50);
     }
 
         /*
@@ -44,7 +63,21 @@ public class Assignment1 {
         Jack who scored 400,
         James who scored 50
          */
-
+public static void displayHighScorePosition(String playerName, int score){
+int position= calulateHighScorePosition(score);
+    System.out.println(playerName + " managed to get into position " + position);
+}
+public static int calulateHighScorePosition(int playerScore){
+    if(playerScore>=1000){
+        return 1;
+    }else if(playerScore>=500 && playerScore<1000){
+        return 2;
+    }else if(playerScore>=100 && playerScore<500){
+        return 3;
+    }else{
+        return 4;
+    }
+}
 
         /*
         Part 5:
