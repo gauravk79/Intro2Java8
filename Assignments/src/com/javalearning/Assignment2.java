6a4f85a560c6ac3653025d280b5bf84e89a6ec10
gaurav khandpur <gauravk79@gmail.com>
2018-01-15 18:00:31 -0800

    added part3 to assignment2

--- Assignments/src/com/javalearning/Assignment2.java
+++ Assignments/src/com/javalearning/Assignment2.java
@@ -24,5 +24,20 @@ public class Assignment2 {
 
          */
 
+        /*
+        Part3: Using loops, write code that will output to screen the following:
+        1
+        1 3
+        1 3 5
+        1 3 5 7
+        1 3 5 7 9
+        1 3 5 7 9
+        1 3 5 7
+        1 3 5
+        1 3
+        1
+         */
+
+
     }
 }
